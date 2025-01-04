a3912e71ec5e723f7aa0e3818e66fe1faa41a0c7
TimofiySumka <timofegu.khmnu@gmail.com>
2025-01-04 18:57:38 +0200

    LogTest

--- app/src/main/java/com/example/teabuddy/MainActivity.kt
+++ app/src/main/java/com/example/teabuddy/MainActivity.kt
@@ -1,6 +1,10 @@
 package com.example.teabuddy
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -15,6 +19,17 @@ class MainActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+        val label= findViewById<TextView>(R.id.LogText)
+        val userName: EditText = findViewById(R.id.LogName)
+        val logButton: Button = findViewById(R.id.LogButton)
+
+        logButton.setOnClickListener{
+            var text = userName.text.toString().trim()
+            if (text.isEmpty())
+                Toast.makeText(this,"Заповніть поле з вашем ім'ям.",Toast.LENGTH_SHORT).show()
+            else
+                label.setText("Вітаємо,"+text)
         }
     }
 }
